a03f97511f319da7bb2165b2cc6f400876d4d9e6
Vlad <ricean.vlad7@gmail.com>
2024-02-29 09:53:07 +0200

    Shoot toward mouse

--- Assets/Scripts/DeagleScript.cs
+++ Assets/Scripts/DeagleScript.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class DeagleScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject bulletPrefab; // Assign in inspector
+    public Transform shootPoint; // Assign a point from where bullets will be instantiated
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Make weapon follow mouse
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePosition - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        // Shoot bullet towards mouse direction
+        if (Input.GetButtonDown("Fire1")) // Default left mouse button
+        {
+            ShootBullet(direction.normalized);
+        }
+    }
+
+    void ShootBullet(Vector2 direction)
+    {
+        if (bulletPrefab && shootPoint)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            if (rb)
+            {
+                rb.AddForce(direction * 1000); // Adjust force as needed
+            }
+        }
     }
 }
